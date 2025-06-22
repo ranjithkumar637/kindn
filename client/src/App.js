880a377829055e8daa87e33a218a02facda81719
RanjithKumar A <82988385+ranjithkumar637@users.noreply.github.com>
2025-06-22 14:04:25 +0530

    Issue fixed

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ const RequesterDashboard = () => <div><h2>Help Seeker Dashboard</h2></div>;
 const PrivateRoute = ({ children, roles }) => {
   const { user } = useContext(AuthContext);
   if (!user) return <Navigate to="/login" />;
-  if (roles && !roles.includes(user.role)) return <Navigate to="/" />;
+  if (roles && !roles.includes(user.user.role)) return <Navigate to="/" />;
   return children;
 };
 
